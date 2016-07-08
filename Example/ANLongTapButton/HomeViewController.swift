fc326a3b81fb4b9f439d73ccd6056fefe9c5f17f
Sergey Demchenko <antrix1989@gmail.com>
2016-07-08 11:19:29 -0700

    Added home screen.

---
+++ Example/ANLongTapButton/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  ANLongTapButton
+//
+//  Created by Sergey Demchenko on 7/8/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController
+{
+}
