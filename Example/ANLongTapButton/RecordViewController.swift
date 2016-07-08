1559fd0efdf86b55bb4243ed5643b19f1eba8e0c
Sergey Demchenko <antrix1989@gmail.com>
2016-07-08 11:25:10 -0700

    Added empty RecordViewController.

---
+++ Example/ANLongTapButton/RecordViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecordViewController.swift
+//  ANLongTapButton
+//
+//  Created by Sergey Demchenko on 7/8/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController
+{
+}
